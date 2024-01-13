0e08c4de8c0b96b99263d39b90a70901c36f88e7
Vinicius Gabriel <vinigpo@gmail.com>
2024-01-12 21:33:16 -0300

    Adiciona busca automatica por cep

--- lib/pages/register/services/cep-service.dart
+++ lib/pages/register/services/cep-service.dart
@@ -8,9 +8,14 @@ Future<Cep> getCep(String? cep) async {
   var response = await http.get(url);
   if (response.statusCode == 200) {
     Map<String, dynamic> jsonResponse = json.decode(response.body);
-    Cep cep = Cep.fromJson(jsonResponse);
-    return cep;
+    if (jsonResponse.length > 1) {
+      Cep cep = Cep.fromJson(jsonResponse);
+      return cep;
+    } else {
+      return Cep("", "", "", "", "", "", "", "", "", "");
+    }
   } else {
-    return Future.error("CEP inválido");
+    return Cep("", "", "", "", "", "", "", "", "", "");
+    ;
   }
 }
