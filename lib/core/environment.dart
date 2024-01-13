e62826d801019753939860d446f8fade1455aca3
Vinicius Gabriel <vinigpo@gmail.com>
2024-01-13 17:14:01 -0300

    Rota provisotira

--- lib/core/environment.dart
+++ lib/core/environment.dart
@@ -1,7 +1,8 @@
 //Routes for the API - Android
-late String loginUrl = 'http://10.0.2.2:8080/auth/login';
-late String activeUserUrl = 'http://10.0.2.2:8080/user/active';
-late String userUrl = 'http://10.0.2.2:8080/user';
+late String loginUrl = 'https://bf1f-177-52-117-143.ngrok-free.app/auth/login';
+late String activeUserUrl =
+    'https://bf1f-177-52-117-143.ngrok-free.app/user/active';
+late String userUrl = 'https://bf1f-177-52-117-143.ngrok-free.app/user';
 late String viaCepUrl = 'https://viacep.com.br/ws/';
 
 //Routes for the API - IOs
