ce3058a986b21bfe494c7d321b9c31925c86b328
Vinicius <vinigpo@gmail.com>
2024-03-14 12:32:55 -0300

    Adiciona lista de vagas proximas

--- lib/pages/home/home-page.dart
+++ lib/pages/home/home-page.dart
@@ -45,66 +45,70 @@ class _HomePageState extends State<HomePage> {
             fit: BoxFit.fill,
           ),
         ),
-        child: ListView.builder(
-          itemCount: users.length + 1,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return Column(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
                 children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(
-                    children: [
-                      const SizedBox(width: 35),
-                      urlFotoPerfil != ''
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                  screenSize.width * 0.3 / 2),
-                              child: Image.network(
-                                urlFotoPerfil,
-                                width: screenSize.width * 0.115,
-                                height: screenSize.width * 0.115,
-                                fit: BoxFit.cover,
-                              ),
-                            )
-                          : CircularProgressIndicator(),
-                      const SizedBox(width: 230),
-                      CustomPopupMenu()
-                    ],
-                  ),
-                  const SizedBox(height: 35),
-                  SizedBox(
-                    width: screenSize.width * 0.9,
-                    child: SearchBar(
-                      controller: controller,
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                        EdgeInsets.symmetric(horizontal: 16.0),
-                      ),
-                      onTap: () {
-                        controller.openView();
-                      },
-                      onChanged: (_) {
-                        controller.openView();
-                      },
-                      leading: const Icon(Icons.search),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
+                  const SizedBox(width: 35),
+                  urlFotoPerfil != ''
+                      ? ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(screenSize.width * 0.3 / 2),
+                          child: Image.network(
+                            urlFotoPerfil,
+                            width: screenSize.width * 0.115,
+                            height: screenSize.width * 0.115,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : const CircularProgressIndicator(),
+                  const SizedBox(width: 230),
+                  const CustomPopupMenu()
                 ],
-              );
-            } else {
-              return ListTile(
-                title: Text(
-                  users[index - 1].nome,
+              ),
+              const SizedBox(height: 35),
+              SizedBox(
+                width: screenSize.width * 0.9,
+                child: SearchBar(
+                  controller: controller,
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  "Vagas próximas a você",
                   style: TextStyle(color: Colors.white),
                 ),
-                onTap: () {
-                  context.go("/perfil", extra: users[index - 1]);
-                },
-              );
-            }
-          },
+              ),
+              const SizedBox(height: 10),
+              ...users
+                  .map((user) => SizedBox(
+                      width: screenSize.width * 0.9,
+                      child: Card(
+                          child: ListTile(
+                        title: Text(
+                          user.nome,
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                        subtitle: Text(user.id),
+                      ))))
+                  .toList(),
+            ],
+          ),
         ),
       ),
     );
