634ebf4550e41d502a9501d905cf6b6c06fd2aed
Vinicius <vinigpo@gmail.com>
2024-03-23 19:01:36 -0300

    Adiciona interceptor

--- lib/pages/initial/services/initial-page-service.dart
+++ lib/pages/initial/services/initial-page-service.dart
@@ -2,20 +2,22 @@ import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:conectatrabalho/core/environment.dart';
+import 'package:conectatrabalho/core/http/validate-token.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/user-model.dart';
 
 Future<User> getUser(String? id) async {
+  Dio dio = Dio();
+  dio.interceptors.add(TokenInterceptor(dio));
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  var url = Uri.parse(userUrl + "/" + id!);
-  var response = await http.get(url,
-      headers: {"Authorization": "Bearer ${prefs.getString('accessToken')}"});
+  var url = userUrl + "/" + id!;
+  var response = await dio.get(url);
   if (response.statusCode == 200) {
-    Map<String, dynamic> jsonResponse = json.decode(response.body);
-    if (jsonResponse.length > 1) {
-      User user = User.fromJson(jsonResponse);
+    if (response.data.length > 1) {
+      User user = User.fromJson(response.data);
       return user;
     } else {
       return User("", "", 0);
@@ -26,18 +28,18 @@ Future<User> getUser(String? id) async {
 }
 
 Future<bool> userWithProfile(String? id) async {
+  Dio dio = Dio();
+  dio.interceptors.add(TokenInterceptor(dio));
   SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool containProfile = false;
 
-  var url = Uri.parse("$userUrl/${id!}/tem-perfil-cadastrado");
-  var response = await http.get(url,
-      headers: {"Authorization": "Bearer ${prefs.getString('accessToken')}"});
-  if (response.statusCode == 200) {
-    if (response.body == "true") {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
+  var url = "$userUrl/${id!}/tem-perfil-cadastrado";
+  await dio
+      .get(url)
+      .then((value) => {containProfile = value.data})
+      .catchError((e) {
+    containProfile = false;
+  });
+
+  return false;
 }
